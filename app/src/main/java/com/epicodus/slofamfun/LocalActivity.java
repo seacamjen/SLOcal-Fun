058676c3aebbbd800a825134ac07dd7f0b220486
Cameron Jensen <jensenseanc@gmail.com>
2017-05-26 14:45:44 -0700

    add validation for form

--- app/src/main/java/com/epicodus/slofamfun/LocalActivity.java
+++ app/src/main/java/com/epicodus/slofamfun/LocalActivity.java
@@ -1,9 +1,11 @@
 package com.epicodus.slofamfun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -12,6 +14,7 @@ import butterknife.ButterKnife;
 
 public class LocalActivity extends AppCompatActivity {
     @Bind(R.id.localList) ListView mLocalList;
+    @Bind(R.id.searchedTextView) TextView mSearchedTextView;
     private String[] activities = new String[] {"Bishops Peak", "Montana De Oro", "SLO Childrens Museum", "Avila Valley Barn", "Atascadero Zoo"};
     private String[] cities = new String[] {"San Luis Obispo", "Los Osos", "San Luis Obispo", "Avila Beach", "Atascadero"};
 
@@ -20,6 +23,10 @@ public class LocalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local);
         ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String search = intent.getStringExtra("search");
+        mSearchedTextView.setText("Here are the results filtered by: " + search);
 
         LocalActivitiesArrayAdapter adapter = new LocalActivitiesArrayAdapter(this, android.R.layout.simple_list_item_1, activities, cities);
         mLocalList.setAdapter(adapter);
--- app/src/main/java/com/epicodus/slofamfun/MainActivity.java
+++ app/src/main/java/com/epicodus/slofamfun/MainActivity.java
@@ -5,6 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -15,17 +20,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Bind(R.id.aboutButton) Button mAboutButton;
     @Bind(R.id.homeButton) Button mHomeButton;
     @Bind(R.id.searchButton) Button mSearchButton;
+    @Bind(R.id.searchInput) EditText mSearchInput;
+    private AwesomeValidation awesomeValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        awesomeValidation.addValidation(this, R.id.searchInput, "^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$", R.string.searcherror);
+
         mYelpButton.setOnClickListener(this);
         mLocalButton.setOnClickListener(this);
         mAboutButton.setOnClickListener(this);
         mHomeButton.setOnClickListener(this);
         mSearchButton.setOnClickListener(this);
+    }
+
+    private void submitForm() {
+        if(awesomeValidation.validate()) {
+            Toast.makeText(this, "Validation Successful", Toast.LENGTH_SHORT).show();
+
+            String search = mSearchInput.getText().toString();
+            Intent intent = new Intent(MainActivity.this, LocalActivity.class);
+            intent.putExtra("search", search);
+            startActivity(intent);
+        }
     }
 
     @Override
@@ -43,10 +66,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(MainActivity.this, MainActivity.class);
             startActivity(intent);
         } else if (v == mSearchButton) {
-            String search = mSearchButton.getText().toString();
-            Intent intent = new Intent(MainActivity.this, LocalActivity.class);
-            intent.putExtra("search", search);
-            startActivity(intent);
+            submitForm();
         }
     }
 }
