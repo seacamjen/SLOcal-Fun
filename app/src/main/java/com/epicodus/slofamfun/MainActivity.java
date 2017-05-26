fe3e2a48690985df7ad18d92c3d4f5130ea1968c
Cameron Jensen <jensenseanc@gmail.com>
2017-05-26 09:06:37 -0700

    add buttons to main activity to go to three other activities

---
+++ app/src/main/java/com/epicodus/slofamfun/MainActivity.java
@@ -0,0 +1,41 @@
+package com.epicodus.slofamfun;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.yelpButton) Button mYelpButton;
+    @Bind(R.id.localButton) Button mLocalButton;
+    @Bind(R.id.aboutButton) Button mAboutButton;
+    @Bind(R.id.homeButton) Button mHomeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mYelpButton) {
+            Intent intent = new Intent(MainActivity.this, YelpActivity.class);
+            startActivity(intent);
+        } else if (v == mLocalButton) {
+            Intent intent = new Intent(MainActivity.this, LocalActivity.class);
+            startActivity(intent);
+        } else if (v == mAboutButton) {
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
+        } else if (v == mHomeButton) {
+            Intent intent = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+}
