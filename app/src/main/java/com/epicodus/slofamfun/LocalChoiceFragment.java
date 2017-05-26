2e62a0f99a369f2b59e5913b5e0ee2e56339d26d
Cameron Jensen <jensenseanc@gmail.com>
2017-05-26 16:31:01 -0700

    add fragment to yelpactivity to go to localactivity

---
+++ app/src/main/java/com/epicodus/slofamfun/LocalChoiceFragment.java
@@ -0,0 +1,37 @@
+package com.epicodus.slofamfun;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocalChoiceFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("SLOcal Family Fun");
+        builder.setMessage("Would you like to see local's favorites?");
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(getActivity(), LocalActivity.class);
+                startActivity(intent);
+                dismiss();
+            }
+        });
+
+        return builder.create();
+    }
+}
--- app/src/main/java/com/epicodus/slofamfun/YelpActivity.java
+++ app/src/main/java/com/epicodus/slofamfun/YelpActivity.java
@@ -12,13 +12,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class YelpActivity extends AppCompatActivity {
+public class YelpActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.localActivites) Button mLocalActivites;
     @Bind(R.id.yelpList) ListView mYelpList;
     private String[] kidActivity = new String[] {"SLO Childrens Museum", "Mitchell Park", "Pismo Beach", "Avila Beach", "Bishop Peak"};
 
@@ -30,6 +32,17 @@ public class YelpActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, kidActivity);
         mYelpList.setAdapter(adapter);
+
+        mLocalActivites.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mLocalActivites) {
+            FragmentManager fm = getFragmentManager();
+            LocalChoiceFragment localChoiceFragment = new LocalChoiceFragment();
+            localChoiceFragment.show(fm, "Sample Fragment");
+        }
     }
 
 }
