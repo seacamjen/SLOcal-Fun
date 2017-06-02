97a8bcd58d9f1ad10226d4b758067b183c82f92a
Cameron Jensen <jensenseanc@gmail.com>
2017-06-02 08:52:27 -0700

    add icons to folder and fragment page

---
+++ app/src/main/java/com/epicodus/slofamfun/ui/YelpDetailActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.slofamfun.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.slofamfun.R;
+
+public class YelpDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yelp_detail);
+    }
+}
